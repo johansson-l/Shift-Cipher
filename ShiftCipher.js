9f6cc664cb722b7e02ff2fee5a07acd56cfd8659
Linus <lsjn88@gmail.com>
2022-01-24 21:38:39 +0100

    initial commit

---
+++ ShiftCipher.js
@@ -0,0 +1,13 @@
+class ShiftCipher {
+    constructor(shift) {
+        this.shift = shift;
+    }
+    encrypt(string) {
+        
+    }
+    decrypt(string) {
+
+    }
+}
+
+const cipher = new ShiftCipher(2);
